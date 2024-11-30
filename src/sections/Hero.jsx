2efa36c286dc2f91260e8957820de92db9605415
Pranaav Iyer <pranaav.iyer@gmail.com>
2024-11-29 19:07:04 -0800

    heroUpdate

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,22 +1,49 @@
+import Typewriter from 'typewriter-effect';
+
 const Hero = () => {
   return (
     <div className="relative left-[3%] sm:left-[0px] xs:left-[0px] bottom-[50%] max-container font-inter flex justify-center items-center w-full xl:flex-row flex-col my-5 xl:left-[50px] xl:px-20">
       
       <div className="p-2 md:p-0 flex flex-col items-center mt-[135px]">
           <div className="flex flex-row items-center text-center xl:text-[50px] md:text-[40px] sm:text-[30px] xs:text-[25px] font-bold opacity-100">
-            <span className="md:ml-1 xl:ml-4 ml-2 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">Hello, I'm Pranaav Iyer</span>
+            <span className="md:ml-1 xl:ml-4 ml-2 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">
+            <Typewriter
+                onInit={(typewriter) => {
+                    typewriter.typeString("Hello, I'm Pranaav Iyer")
+                    .changeDelay(1)
+                    .changeDeleteSpeed(5)
+                    .start();
+                }}
+            />
+            </span>
           </div>
           <div className="flex flex-row items-center text-center xl:text-[50px] md:text-[40px] sm:text-[30px] xs:text-[25px] font-bold opacity-100 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">
-            <span className="md:ml-1 xl:ml-4 ml-2">I</span>
-            <span className="md:ml-1 xl:ml-4 ml-2">explore,</span>
-            <span className="md:ml-1 xl:ml-4 ml-2">design,</span>
-            <span className="md:ml-1 xl:ml-4 ml-2">and</span>
-            <span className="md:ml-1 xl:ml-4 ml-2">craft</span>
-            <span className="md:ml-1 xl:ml-4 ml-2">innovative</span>
-            <span className="md:ml-1 xl:ml-4 ml-2">software.</span>
+            <span className="md:ml-1 xl:ml-4 ml-2">
+            <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .changeDelay(50)
+                    .pauseFor(4200)
+                    .changeDeleteSpeed(5)
+                    .typeString("I find,")
+                    .pauseFor(500)
+                    .deleteChars(5)
+                    .typeString('plan,')
+                    .pauseFor(500)
+                    .deleteChars(5)
+                    .typeString('create,')
+                    .pauseFor(1000)
+                    .changeDeleteSpeed(15)
+                    .deleteChars(7)
+                    .pauseFor(750)
+                    .typeString('explore, design, and craft innovative software.')
+                    .start();
+                }}
+            />
+            </span>
           </div>
           <div className="flex flex-row items-center text-center xl:text-[25px] md:text-[20px] sm:text-[19px] font-normal opacity-50 px-60 sm:px-0 xs:px-0">
-            <span className="md:ml-1 xl:ml-4 ml-2 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">I'm a driven developer who enjoys transforming complex code into user-friendly solutions, inspired by a passion for innovation and problem-solving.</span>
+            <span className="md:ml-4 xl:ml-4 ml-2 px-10 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">I'm a driven developer who enjoys transforming complex code into user-friendly solutions, inspired by a passion for innovation and problem-solving.</span>
           </div>
         </div>
         <ul className="absolute bg-gray-100 dark:bg-white flex items-center justify-center p-1 rounded-3xl shadow-sm sm:mt-[460px] xs:mt-[360px] xs:scale-75 sm:scale-100">
@@ -34,7 +61,7 @@ const Hero = () => {
         </a>
       </ul>
       <button className="absolute sm:mt-[670px] xs:mt-[550px] gradient-border m-1 rounded-xl flex items-center justify-center dark:text-black bg-black dark:bg-white hover:bg-white dark:hover:bg-neutral-800 hover:text-black dark:hover:text-white opacity-95 hover:opacity-100 text-white px-5 py-3 md:px-10 md:py-5 text-xl transform transition duration-500 hover:scale-110">
-        <a target="_blank" href="/PranaavIyer_Resume.docx">Resume</a>
+        <a target="_blank" href="/PranaavIyer_Resume.pdf">Resume</a>
       </button>
 
     </div>
