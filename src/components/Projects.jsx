520cea174a5fa9c07ecb05e188ba58fc42dd1a5d
Pranaav Iyer <pranaav.iyer@gmail.com>
2024-12-10 13:05:11 -0500

    fixedMobileView

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -61,35 +61,65 @@ const projectsData = [
     // Add more projects as needed
 
 const Projects = () => {
+    const isMobile = window.innerWidth <= 480;
+
     return (
         <div id="portfolio" className="relative text-center w-full flex flex-col items-center font-inter mt-[-150px] lg:mt-[-250px] md:mt-[-250px]">
             <div className="flex flex-wrap gap-6 justify-center mt-[-170px] md:mt-[-130px]">
                 {projectsData.map((project, index) => (
-                    <Tilt options={defaultOptions} key={index}>
-                        <motion.div 
-                            className="relative border rounded-lg shadow-lg p-4 w-80 group transform transition-transform duration-300 hover:scale-105"
-                            initial={{ opacity: 0, y: 50 }}
-                            whileInView={{ opacity: 1, y: 0 }}
-                            viewport={{ once: true }} // Ensures the animation runs only once
-                            transition={{ duration: 0.5, delay: index * 0.3 }} // Reduced delay for faster loading
-                        >
-                            <div className="relative overflow-hidden rounded-t-lg">
-                                <img src={project.imageUrl} alt={project.title} className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-110" />
-                                <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-75"></div>
-                            </div>
-                            <div className="p-4">
-                                <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-                                <p className="text-gray-400 text-sm">{project.date}</p>
-                            </div>
-                            <div className="absolute inset-0 bg-gray-800 rounded-lg bg-opacity-90 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4">
-                                <p className="text-xl font-semibold text-white">{project.hovertitle}</p>
-                                <p className="text-white mt-2">{project.description}</p>
-                                <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline pt-3 mb-2 block">
-                                    <button className="bg-white text-[#404040] px-3 py-1 rounded">View Project</button>
-                                </a>
-                            </div>
-                        </motion.div>
-                    </Tilt>
+                    isMobile ? (
+                        <div key={index} className="relative border rounded-lg shadow-lg p-4 w-80 group transform transition-transform duration-300 hover:scale-105">
+                            <motion.div 
+                                className="relative border rounded-lg shadow-lg p-4 w-80 group transform transition-transform duration-300 hover:scale-105"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                viewport={{ once: true }} // Ensures the animation runs only once
+                                transition={{ duration: 0.5, delay: index * 0.3 }} // Reduced delay for faster loading
+                            >
+                                <div className="relative overflow-hidden rounded-t-lg">
+                                    <img src={project.imageUrl} alt={project.title} className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-110" />
+                                    <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-75"></div>
+                                </div>
+                                <div className="p-4">
+                                    <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+                                    <p className="text-gray-400 text-sm">{project.date}</p>
+                                </div>
+                                <div className="absolute inset-0 bg-gray-800 rounded-lg bg-opacity-90 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4">
+                                    <p className="text-xl font-semibold text-white">{project.hovertitle}</p>
+                                    <p className="text-white mt-2">{project.description}</p>
+                                    <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline pt-3 mb-2 block">
+                                        <button className="bg-white text-[#404040] px-3 py-1 rounded">View Project</button>
+                                    </a>
+                                </div>
+                            </motion.div>
+                        </div>
+                    ) : (
+                        <Tilt options={defaultOptions} key={index}>
+                            <motion.div 
+                                className="relative border rounded-lg shadow-lg p-4 w-80 group transform transition-transform duration-300 hover:scale-105"
+                                initial={{ opacity: 0, y: 50 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                viewport={{ once: true }} // Ensures the animation runs only once
+                                transition={{ duration: 0.5, delay: index * 0.3 }} // Reduced delay for faster loading
+                            >
+                                <div className="relative overflow-hidden rounded-t-lg">
+                                    <img src={project.imageUrl} alt={project.title} className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-110" />
+                                    <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-75"></div>
+                                </div>
+                                <div className="p-4">
+                                    <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+                                    <p className="text-gray-400 text-sm">{project.date}</p>
+                                </div>
+                                <div className="absolute inset-0 bg-gray-800 rounded-lg bg-opacity-90 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4">
+                                    <p className="text-xl font-semibold text-white">{project.hovertitle}</p>
+                                    <p className="text-white mt-2">{project.description}</p>
+                                    <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline pt-3 mb-2 block">
+                                        <button className="bg-white text-[#404040] px-3 py-1 rounded">View Project</button>
+                                    </a>
+                                </div>
+                            </motion.div>
+                        </Tilt>
+                    )
                 ))}
             </div>
         </div>
--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,10 +1,10 @@
 const Projects = () => {
   return (
-    <div id="portfolio" class="relative text-center w-[100%] flex flex-col items-center font-inter mt-[-130px] lg:mt-[-250px] md:mt-[-250px]">
+    <div id="portfolio" className="absolute text-center w-[100%] flex flex-col items-center font-inter mt-[-130px] xxs:hidden lg:mt-[-250px] md:mt-[-250px]">
         <h2 className="relative md:text-4xl sm:text-3xl xs:text-2xl font-black">
             Selected Portfolio
-            </h2>
-        </div>
+        </h2>
+    </div>
   )
 }
 
