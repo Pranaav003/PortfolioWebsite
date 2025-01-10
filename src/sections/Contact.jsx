cfa1c209a0e08cd7ef7465113ac6286239057955
Pranaav <pranaav.iyer@gmail.com>
2025-01-09 21:42:51 -0800

    Minor fix (#8)
    * fixedMobileView
    
    * MobileViewFix2
    
    * lightmodefix
    
    * linkFix
    
    * minor fix

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -3,7 +3,7 @@ import ContactBoxes from '../components/ContactBoxes';
 const Contact = () => {
     return (
         <div id="contact" className="w-[100%] text-center flex flex-col items-center font-inter">
-                <h2 className="relative text-4xl sm:bottom-[200px] xs:bottom-[50px] font-black">Contact Me</h2>
+                <h2 className="relative text-4xl sm:bottom-[200px] xs:bottom-[50px] xxs:bottom-[-140px] font-black">Contact Me</h2>
         </div>
     )
 }
