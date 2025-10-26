33396028bfa7261498e677ac75f84edaabc2da95
Pranaav Iyer <pranaav.iyer@gmail.com>
2025-10-25 20:00:26 -0400

    iphoneresponsiveness

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,11 +2,11 @@ import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
   return (
-    <div className="relative left-[3%] sm:left-[0px] xs:left-[0px] bottom-[50%] max-container font-inter flex justify-center items-center w-full xl:flex-row flex-col my-5 xl:left-[50px] xl:px-20">
+    <div className="relative left-0 sm:left-0 bottom-[50%] max-container font-inter flex justify-center items-center w-full xl:flex-row flex-col my-5 xl:left-[50px] xl:px-20">
       
-      <div className="p-2 md:p-0 flex flex-col items-center mt-[135px]">
+      <div className="p-2 md:p-0 flex flex-col items-center mt-[135px] sm:mt-[135px] xs:mt-[100px]">
           <div className="flex flex-row items-center text-center xl:text-[50px] md:text-[40px] sm:text-[30px] xs:text-[20px] font-bold opacity-100">
-            <span className="md:ml-1 xl:ml-4 ml-2 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">
+            <span className="md:ml-1 xl:ml-4 ml-0 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">
             <Typewriter
                 onInit={(typewriter) => {
                     typewriter.typeString("Hello, I'm Pranaav Iyer")
@@ -18,7 +18,7 @@ const Hero = () => {
             </span>
           </div>
           <div className="flex flex-row items-center text-center xl:text-[50px] md:text-[40px] sm:text-[30px] xs:text-[20px] font-bold opacity-100 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">
-            <span className="md:ml-1 xl:ml-4 ml-2">
+            <span className="md:ml-1 xl:ml-4 ml-0">
             <Typewriter
                 onInit={(typewriter) => {
                   typewriter
@@ -42,8 +42,8 @@ const Hero = () => {
             />
             </span>
           </div>
-          <div className="flex flex-row items-center text-center xl:text-[25px] md:text-[20px] sm:text-[19px] xs:text-[15px] font-normal opacity-50 px-10 sm:px-0 xs:px-0">
-            <span className="md:ml-4 xl:ml-4 ml-2 px-5 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">I'm a driven developer who enjoys transforming complex code into user-friendly solutions, inspired by a passion for innovation and problem-solving.</span>
+          <div className="flex flex-row items-center text-center xl:text-[25px] md:text-[20px] sm:text-[19px] xs:text-[15px] font-normal opacity-50 px-4 sm:px-0">
+            <span className="md:ml-4 xl:ml-4 ml-0 animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-linear">I'm a driven developer who enjoys transforming complex code into user-friendly solutions, inspired by a passion for innovation and problem-solving.</span>
           </div>
         </div>
         <ul className="absolute xxs:mt-[500px] bg-gray-100 dark:bg-white flex items-center justify-center p-1 rounded-3xl shadow-sm sm:mt-[460px] xs:mt-[360px] xs:scale-75 sm:scale-100">
